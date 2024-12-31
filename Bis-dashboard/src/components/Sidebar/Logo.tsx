44e122695a970bec61e266782e0c66235b9e5453
Naresh <nareshm4243@gmail.com>
2024-12-31 08:06:11 +0530

    4th commit

--- Bis-dashboard/src/components/Sidebar/Logo.tsx
+++ Bis-dashboard/src/components/Sidebar/Logo.tsx
@@ -4,6 +4,6 @@ import { Target } from 'lucide-react';
 export const Logo: React.FC = () => (
   <div className="flex items-center gap-2 px-3 mb-8">
     <Target className="w-8 h-8 text-white" />
-    <h1 className="text-xl font-bold text-white">BIS Arena</h1>
+    <h1 className="text-xl font-bold text-white">BIZ Hack Hub</h1>
   </div>
 );
